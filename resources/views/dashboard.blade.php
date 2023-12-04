1bf34e5681c31a110a4eee7f7f901f4525e34fb3
Sreyas S <sreyassivadasan07@gmail.com>
2023-12-04 09:27:29 +0530

    Removed Dashboard Logo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,4 +14,13 @@
             </div>
         </div>
     </div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <a href="{{route('task.index')}}" class="p-6 text-blue-500 hover:text-blue">Tasks Page</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
